09ca30ea1e0d2e7dfd43d186d31aedc7bb0b10d4
SantiagoOsorio4 <santiagoosoriocordoba@gmail.com>
2023-04-27 00:14:28 -0500

    cambios2

--- resources/views/Paciente/index.blade.php
+++ resources/views/Paciente/index.blade.php
@@ -11,7 +11,19 @@
     <title>Listado de pacientes</title>
   </head>
   <body>
+  <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Communes') }}
+        </h2>
+    </x-slot>
+
     <div class="container">
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+
     <h1>Listado de Pacientes</h1>
     <a href="{{ route('paciente.create') }}" class="btn btn-success">Registrar nuevo paciente</a>
     <table class="table">
@@ -49,7 +61,11 @@
     @endforeach
   </tbody>
 </table>
-
+</x-app-layout>
+    </div>
+    </div>
+    </div>
+    </div>
     </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
